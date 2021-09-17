14fc9b4957a9581e8afd130a6b19b41b7637b5b8
unknown <ekdp111@inha.edu>
2021-09-17 13:46:20 +0900

    v0.5

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-// v0.3
+// v0.5
 public class ReviewDemo01 {
     public static void main(String[] args) {
         // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -7,9 +7,22 @@ public class ReviewDemo01 {
         int number = sc.nextInt();
         boolean isPrime = true;
 
-        for(int k=2; k<number; k++){
-            if(number % k == 0) // 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
+//        for(int k=2; k<number; k++){
+//            if(number % k == 0) {
+//                isPrime = false;
+//                break; // 첫 번째 약수가 발견되면 for문 탈출
+//            }
+//            //System.out.println(k);
+//        }
+
+        int k=2;
+        while (k<number){
+            if(number % k == 0) {
                 isPrime = false;
+                break; // 첫 번째 약수가 발견되면 for문 탈출
+            }
+            //System.out.println(k);
+            k++;
         }
 
         if(isPrime) // isPrime 변수 값이 true면
@@ -18,6 +31,27 @@ public class ReviewDemo01 {
             System.out.println(number + "은(는) 소수가 아닙니다.");
     }
 }
+
+//import java.util.Scanner;
+//// v0.3
+//public class ReviewDemo01 {
+//    public static void main(String[] args) {
+//        // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
+//        Scanner sc = new Scanner(System.in);
+//        int number = sc.nextInt();
+//        boolean isPrime = true;
+//
+//        for(int k=2; k<number; k++){
+//            if(number % k == 0) // 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
+//                isPrime = false;
+//        }
+//
+//        if(isPrime) // isPrime 변수 값이 true면
+//            System.out.println(number + "은(는) 소수!");
+//        else
+//            System.out.println(number + "은(는) 소수가 아닙니다.");
+//    }
+//}
 
 //import java.util.Scanner;
 //// v0.2
